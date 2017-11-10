1b975418bdce1e8e5d7e0f3466a589a5e8ddea71
neo <50157272@qq.com>
2017-11-10 18:06:49 +0800

    Update web1.js

--- web1.js
+++ web1.js
@@ -5,6 +5,6 @@ var os = require('os');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('<h1>Hello World</h1>'+os.hostname()+'\n');
-}).listen(8888);
+}).listen(1500);
 // 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://127.0.0.1:1500/');
